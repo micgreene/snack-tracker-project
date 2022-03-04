015beef0c097d8efd16cf3b2351fff51c372c05d
micgreene <mgreene11111@yahoo.com>
2022-03-03 22:40:33 -0800

    updating tests.py for our snacks app

--- snack-tracker-project/snacks/tests.py
+++ snack-tracker-project/snacks/tests.py
@@ -1,13 +1,31 @@
-from django.test import SimpleTestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase
 from django.urls import reverse
+from snacks.models import Snack
 
-class SnacksTests(SimpleTestCase):
-  def test_home_page_status_code(self):
-    url = reverse('snack_list')
-    response = self.client.get(url)
-    self.assertEqual(response.status_code, 200)
+# Create your tests here.
 
-  def test_home_base_template(self):
-    url = reverse('snack_list')
-    response = self.client.get(url)
-    self.assertTemplateUsed(response, 'base.html')
+class SnacksTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="tester", email="tester@email.com", password="pass")
+        self.snack = Snack.objects.create(
+            name = 'Chips', purchaser = self.user, description = 'Crunchy')
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.movie), 'Chips')
+
+    def test_snack_name(self):
+        self.assertEqual(f'{self.snack.name}', 'Chips')
+
+    def test_list_page_status_code(self):
+        url = reverse("snack_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse("snack_list")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "snack_list.html")
+        self.assertTemplateUsed(response, "base.html")
